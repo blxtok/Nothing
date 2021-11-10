da7e148b8588d1fdc576f386bfa8ba1309dd0d1b
blxtok <89456941+blxtok@users.noreply.github.com>
2021-11-10 18:10:29 +0300

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,9 +1,11 @@
-local script1 = getgenv().script1
-
-if game.PlaceId == getgenv().gameid then
-	print(getgenv().gameid)
-	loadstring(game:HttpGet(script1))()
-	wait(1)
+if game.PlaceId == 2629642516 then
+	getgenv().scriptext1 = "zort"
+	getgenv().scriptext2 = "zort"
+	getgenv().scriptext3 = "none"
+	getgenv().scriptext4 = "none"
+	getgenv().scriptext5 = "none"
+	getgenv().script1 = "https://asdasdds.com"
+	getgenv().script2 = "https://asdasdds.com"
 end
 
 print("zort")
